4aa10004d9a39ee7a12fa2d2df3c498dddd88774
Anna <anioa1998@wp.pl>
2022-01-25 19:22:21 +0100

    Added HomeController.cs

---
+++ oneboxProject/OneBox/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OneBox.Controllers
+{
+    public class HomeController : Controller
+    {
+        //Obsługa kafelka "nadaj paczkę"
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
